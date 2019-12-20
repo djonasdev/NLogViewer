e493e78f924f315d933bf79553d8fbae61271676
Dominic Jonas <djonas@haprotec.de>
2019-12-20 09:11:31 +0100

    added createBranch script for better branch creating

--- createBranch.ps1
+++ createBranch.ps1
@@ -1,8 +1,12 @@
 param([string]$IssueName)
 
-$branchName=$(echo $IssueName | tr '[:upper:]' '[:lower:]' | tr '/' '-' | tr ' ' '-')
+$branchName = $IssueName.ToLower().Replace('/', '-').Replace(' ', '-')
+$branchName = $branchName.Substring('0', '40')
+
+echo "New branch '$branchName' created"
+
 git checkout -b $branchName
 git add .
 git add -u
-git commit -m "Description of my changes for issue $IssueName"
-git push -u origin $branchName
+#git commit -m "Description of my changes for issue $IssueName"
+#git push -u origin $branchName
